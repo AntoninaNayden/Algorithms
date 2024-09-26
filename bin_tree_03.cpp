40844987f70439a1299d0af90a2897fef8954b9f
naydena <121058964+AntoninaNayden@users.noreply.github.com>
2024-09-26 13:34:57 +0300

    Add files via upload

---
+++ bin_tree_03.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cstdio>
+#include<vector>
+#include <limits>
+using namespace std;
+class Node {
+public:
+    long long leftParent;
+    long long rightParent;
+    long long node;
+    Node() {
+        node = 0;
+        leftParent = INT64_MIN;
+        rightParent = INT64_MAX;
+    }
+};
+
+int main()
+{
+    freopen("bst.in", "r", stdin);
+    freopen("bst.out", "w", stdout);
+    int n;
+    scanf("%d", &n);
+    Node * nodes = new Node [n];
+    long long str_of_parent;
+    char left_right;
+    scanf("%lld", &nodes[0].node);
+    bool res = true;
+    for (int i = 1; i < n; i++) {
+        scanf("%lld%lld %c", &nodes[i].node, &str_of_parent, &left_right);
+        int j;
+        if (left_right == 'L') {
+            j = str_of_parent;
+            nodes[i].leftParent = nodes[j - 1].leftParent;
+            nodes[i].rightParent = nodes[j - 1].node - 1;
+        }
+        if (left_right == 'R') {
+            j = str_of_parent;
+            nodes[i].rightParent = nodes[j - 1].rightParent;
+            nodes[i].leftParent = nodes[j - 1].node;
+        }
+        if (nodes[i].node > nodes[i].rightParent || nodes[i].node < nodes[i].leftParent) {
+            res = false;
+            break;
+        }
+    }
+
+    if (res) {
+        printf("%s", "YES");
+    }
+    else {
+        printf("%s", "NO");
+    }
+
+
+}
