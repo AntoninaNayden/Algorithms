7acb071a3c5377b8dee971e9ef4cd28e303eae28
naydena <121058964+AntoninaNayden@users.noreply.github.com>
2024-09-30 12:08:35 +0300

    Add files via upload

---
+++ binom_heap.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include<vector>
+#include <cmath>
+using namespace std;
+vector<int> to_bin(long long x) {
+    vector<int> res;
+    while (x != 0) {
+        int a = x % 2;
+        res.push_back(a);
+       x = x / 2;
+    }
+    return res;
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    long long n;
+    cin >> n;
+    bool is_binom=true;
+        vector<int> resu = to_bin(n);
+        for (int i = 0; i < resu.size(); i++) {
+            if (resu[i] == 1) {
+                cout << i << "\n";
+                is_binom = false;
+            }
+        }
+        if (is_binom) {
+            cout << -1;
+        }
+}
