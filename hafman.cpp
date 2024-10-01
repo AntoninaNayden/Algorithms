46ed58b9b2054632286e918299b21f766365aa9c
naydena <121058964+AntoninaNayden@users.noreply.github.com>
2024-10-01 16:48:43 +0300

    Add files via upload

---
+++ hafman.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include<vector>
+#include <queue>
+
+using namespace std;
+ 
+int main()
+{
+	freopen("huffman.in", "r", stdin);
+	freopen("huffman.out", "w", stdout);
+	
+	int n;
+	cin >> n;
+	vector<long long> myleaves;
+	for (int i = 0; i < n; i++) {
+		long long a;
+		cin >> a;
+		myleaves.push_back(a);
+	}
+	long long result = 0;
+	priority_queue<long long, vector<long long>, greater<long long> > qversh(myleaves.begin(), myleaves.end());
+	for (int i = 0; i < n - 1; i++) {
+		long long Sum;
+		Sum = qversh.top();
+		qversh.pop();
+		Sum += qversh.top();
+		qversh.pop();
+		qversh.push(Sum);
+		result += Sum;
+		if (i == n - 2) {
+			cout << result;
+		}
+	}
+
+}
+
