85866a56fc39c9fb41f50e243837270a0fe190b1
naydena <121058964+AntoninaNayden@users.noreply.github.com>
2024-10-09 22:37:31 +0300

    Add files via upload

---
+++ grafchto.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n, m;
+    cin >> n >> m;
+    vector <vector<int> > mycout;
+    for (int i = 0; i < n; i++) {
+        vector<int> x;
+        x.push_back(0);
+        mycout.push_back(x);
+    }
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        mycout[x - 1][0] += 1;
+        mycout[x-1].push_back(y);
+        mycout[y - 1][0] += 1;
+        mycout[y - 1].push_back(x);
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < mycout[i].size(); j++) {
+            cout << mycout[i][j]<<" ";
+        }
+        cout << "\n";
+    }
+}
+
